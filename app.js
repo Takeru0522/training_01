c2566ae5db50014da8c6a6c827fe68376f72a56b
TAKERU <takeru.kayamori@gmail.com>
2020-02-03 21:17:32 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// $('#read-more').click(function() {
+    // $('.important').css('color', 'red');
+    // $('#more-text').slideDown();
+    // $('#read-more').hide();
+    // $('#image-wrapper').slideUp();
+//   });
+
+
+
+
+// const circle = document.querySelector('#js-circle');
+// new mojs.Tween({
+// 　　repeat: 999,
+// 　　delay: 500,
+// 　　speed: 0.3,
+// 　　onUpdate: function(progress) {
+// 　　　　var bounceProgress = mojs.easing.bounce.out(progress);
+// 　　　　circle.style.transform = 'translateY(' + 200 * bounceProgress + 'px)';
+// 　　}
+// }).play();
+
