dedfad7eaabba3d6805048855042519ed27f4608
TAKERU <takeru.kayamori@gmail.com>
2020-02-07 14:45:38 +0800

    save progress

--- app.js
+++ app.js
@@ -2,10 +2,68 @@ $('.main-carousel').flickity({
     // options
     cellAlign: 'left',
     contain: true
-  });
+});
+
+$('#showcase').click(function() {
+    console.log('clicked')
+});
+
+$('#nav-1').fadeIn()
+// $('.show-texts').hide();
+
+const nav1 = document.getElementById("nav-1"); // -Logo
+const nav2 = document.getElementById("nav-2"); // About
+const nav3 = document.getElementById("nav-3"); // History
+const nav4 = document.getElementById("nav-4"); // Hobby
+const nav5 = document.getElementById("nav-5"); // Contact
+
+// const header = document.getElementById("header");
+const nav = document.getElementById('nav');
+const sectionA = document.getElementById("section-a"); // About
+const sectionB = document.getElementById("section-z"); 
+const sectionC = document.getElementById("section-b");
+const sectionD = document.getElementById("section-c");
+
+// console.log(nav.offsetTop)
 
 
+nav1.addEventListener('click', () => {
+    console.log('clicked')
 
+    nav.scrollIntoView({
+        behavior: "smooth", 
+        block: "start", 
+        inline: "nearest"
+    });
+})
+nav2.addEventListener('click', () => {
+    sectionA.scrollIntoView({
+        behavior: "smooth", 
+        block: "center", 
+        inline: "nearest"
+    });
+})
+nav3.addEventListener('click', () => {
+    sectionB.scrollIntoView({
+        behavior: "smooth", 
+        block: "center", 
+        inline: "nearest"
+    });
+})
+nav4.addEventListener('click', () => {
+    sectionC.scrollIntoView({
+        behavior: "smooth", 
+        block: "center", 
+        inline: "nearest"
+    });
+})
+nav5.addEventListener('click', () => {
+    sectionD.scrollIntoView({
+        behavior: "smooth", 
+        block: "center", 
+        inline: "nearest"
+    });
+})
 
 // $('#read-more').click(function() {
     // $('.important').css('color', 'red');
@@ -28,3 +86,42 @@ $('.main-carousel').flickity({
 // 　　}
 // }).play();
 
+
+$(window).on('load scroll', function(){
+
+    //animatedのclassを持った要素をセレクタに指定
+    const elem = $('.animated');
+    // console.log(elem, 'elem')
+  
+    elem.each(function () {
+
+        //data属性からアニメーション名を取得
+        // const isAnimate = $(this).data('animate');
+        // console.log(isAnimate); // => bounce
+        //animated要素に位置を取得
+        const elemOffset = Math.round($(this).offset().top);
+        // console.log(elemOffset, 'elemOffset');
+        //現在のスクロールポジションを取得
+        const scrollPos = Math.round($(window).scrollTop());
+        // console.log(scrollPos, 'scrollPos')
+        //ウィンドウの高さを取得
+        const wh = $(window).height();
+        // console.log(wh, 'wh')
+
+        // while elem is in the window, keep showing
+        if(scrollPos > elemOffset - wh){
+            $(this).addClass('showUp');
+            $(this).removeClass('showOff');
+        } 
+        // when elem is top of window, elem off
+        if(scrollPos > elemOffset + 75) {
+            $(this).addClass('showOff');
+            $(this).removeClass('showUp');
+        }
+        // keep hiding elem until elem comes into window 
+        if(scrollPos < elemOffset - wh + 75) {
+            $(this).removeClass('showUp');
+            $(this).addClass('showOff');
+        }
+    });
+});
